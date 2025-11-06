e187a5ff412ced2ad04169c2bb1b1c5850c6e28f
shailesh gontewar <shailesh.gontewar@tlminsidesales.com>
2025-11-06 11:29:26 +0530

    done with path change

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
 });
 
 // Body parser middleware
